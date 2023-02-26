65d4da0f35948047c381168126efe498fd7a7f5d
EdNovas <56779951+EdNovas@users.noreply.github.com>
2023-02-25 23:43:43 -0800

    Update emby_notify.py

--- emby_notify.py
+++ emby_notify.py
@@ -1,6 +1,6 @@
 import requests
-import time
 import json
+import sqlite3
 
 EMBY_SERVER_URL = '<Your Emby Server URL>'
 EMBY_API_KEY = '<Your Emby API key>'
@@ -10,55 +10,55 @@ TELEGRAM_BOT_TOKEN = '<Your Telegram Bot>'
 TELEGRAM_CHAT_ID = '<Your Telegram Update Channel>'
 CHANNEL = '<Your channel>'
 GROUP = '<Your group>'
-VIDEO_NAME = ''
 
-def send_telegram_notification(message, photo_url, video_link):
-    url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendPhoto'
-    button = {'text': '点击播放', 'callback_data': 'button_clicked', 'url': video_link}
-    keyboard = [[button]]
-    reply_markup = {'inline_keyboard': keyboard}
-    params = {'chat_id': TELEGRAM_CHAT_ID, 'caption': message, 'photo': photo_url,'reply_markup': json.dumps(reply_markup),}
-    requests.post(url, params=params)
 
-def get_latest_video(user_id):
-    global VIDEO_NAME
-    url = f'{EMBY_SERVER_URL}/emby/Users/{user_id}/Items/Latest?api_key={EMBY_API_KEY}'
-    response = requests.get(url).json()
-    item_id = response[0]['Id']
-    video_name = response[0]['Name']
-    video_type = response[0]['Type']
-    # print(item_id)
-    # print(video_name)
-    print(response[0])
+conn = sqlite3.connect("emby_notify.db")
+conn_cursor = conn.cursor()
+
+# Create the video table if it doesn't already exist
+conn_cursor.execute("""
+    CREATE TABLE IF NOT EXISTS videos (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        type TEXT,
+        img_tag TEXT
+    )
+""")
+
+url = f'{EMBY_SERVER_URL}/emby/Users/{EMBY_USER_ID}/Items/Latest?Limit=2&api_key={EMBY_API_KEY}'
+responses = requests.get(url).json()
+
+for response in responses:
+    item_id = response['Id']
+    item_type = response['Type']
+    item_name = response['Name']
+    item_link = f'{EMBY_SERVER_URL}/web/index.html#!/item?id={item_id}&serverId={SERVER_ID}'
+    
+    additional_url = f'{EMBY_SERVER_URL}/emby/Videos/{item_id}/AdditionalPartsItems&api_key={EMBY_API_KEY}'
+    additional_responses = requests.get(url).json()
+    print(additional_responses)
     try:
-        imageTag = response[0]['ImageTags']['Primary']
-        # print(imageTag)
-        image_url = f'{EMBY_SERVER_URL}/emby/Items/{item_id}/Images/Primary?maxHeight=900&maxWidth=600&tag={imageTag}&quality=90'
-        # print(image_url)
+        item_img = response['ImageTags']['Primary']
+        item_img_url = f'{EMBY_SERVER_URL}/emby/Items/{item_id}/Images/Primary?maxHeight=900&maxWidth=600&tag={item_img}&quality=90'
     except:
-        image_url = 'empty'
-    # print(image_url)
-    video_link = f'{EMBY_SERVER_URL}/web/index.html#!/item?id={item_id}&serverId={SERVER_ID}'
-    # print(video_link)
-    return video_name,video_type, image_url, video_link
+        continue
+    conn_cursor.execute("SELECT COUNT(*) FROM videos WHERE id=?", (item_id,))
+    count = conn_cursor.fetchone()[0]
+    if (count == 0):
+        conn_cursor.execute("INSERT INTO videos (id, name, type, img_tag) VALUES (?, ?, ?, ?)", (item_id, item_name, item_type, item_img))
+        conn.commit()
+        if item_type == 'Movie':
+            item_type_str = '电影'
+        elif item_type == 'Episode':
+            item_type_str = '电视剧'
+        else:
+            item_type_str = 'Unknown'
+        message = f'#上新\nTitle: {item_name} \n类型: {item_type}\n\n频道 {CHANNEL}\n群组 {GROUP}'
+        url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendPhoto'
+        button = {'text': '点击播放', 'callback_data': 'button_clicked', 'url': item_link}
+        keyboard = [[button]]
+        reply_markup = {'inline_keyboard': keyboard}
+        params = {'chat_id': TELEGRAM_CHAT_ID, 'caption': message, 'photo': item_img_url,'reply_markup': json.dumps(reply_markup),}
+        # print(message, item_img_url)
+        requests.post(url, params=params)
 
-def check_for_video_updates():
-    global VIDEO_NAME
-    video_name,video_type, image_url, video_link = get_latest_video(EMBY_USER_ID)
-    # print(VIDEO_NAME)
-    if video_type == 'Movie':
-        video_type_str = '电影'
-    elif video_type == 'Episode':
-        video_type_str = '电视剧'
-    else:
-        video_type_str = 'Unknown'
-    if (image_url != 'empty'):
-        if (video_name != VIDEO_NAME):
-            VIDEO_NAME = video_name
-            message = f'#上新\nTitle: {video_name} \n类型: {video_type_str}\n\n频道 {CHANNEL}\n群组 {GROUP}'
-            print(message)
-            send_telegram_notification(message, image_url, video_link)
-            
-while True:
-    check_for_video_updates()
-    time.sleep(2)
